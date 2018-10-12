0639e1ecba731f95d9e03b873b51ae66e0d54d68
millea <millea1994@gmail.com>
2018-10-12 21:36:28 +0700

    fix typo

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,10 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['api/users']['GET'] 			= "UsersController/all_users";
 $route['api/users/(:num)']['GET']	= "UsersController/detail_user/$1";
 $route['api/register']['POST'] 		= "UsersController/save";
-$route['api/user/(:num)']['PUT'] 	= "UsersController/update/$1';
+$route['api/user/(:num)']['PUT'] 	= "UsersController/update/$1";
 $route['api/user/(:num)']['DELETE'] = "UsersController/delete/$1";
 $route['api/login']					= "UsersController/save";
-
 
 
 $route['default_controller'] = 'welcome';
